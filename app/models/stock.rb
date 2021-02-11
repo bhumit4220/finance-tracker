7c69264d4e0f55d8fd4f77464569496705b49a84
bhumit4220 <bhumit2520@gmail.com>
2021-02-11 17:13:29 +0530

    secure api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
 
     def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(  publishable_token: 'Tpk_3897bde62f0a4adfb2564a1a5cbe6b2e',
+        client = IEX::Api::Client.new(  publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
 
                                             endpoint: 'https://sandbox.iexapis.com/v1')
         begin
