ac931dc5bc06a78141a1ac036e60ef3699060ed9
bhumit4220 <bhumit2520@gmail.com>
2021-02-11 17:03:58 +0530

    add ajax request

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,13 @@
 class Stock < ApplicationRecord
 
     def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+        client = IEX::Api::Client.new(  publishable_token: 'Tpk_3897bde62f0a4adfb2564a1a5cbe6b2e',
+
                                             endpoint: 'https://sandbox.iexapis.com/v1')
-        client.price(ticker_symbol)
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))    
+        rescue => exception
+            return nil
+        end
     end
 end
